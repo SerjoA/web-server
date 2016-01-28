df1634f57db7929f40654d120b180d7fa8b51dc1
Serjo Agronov <SerjoAgronovDev@gmail.com>
2016-01-28 22:53:41 +0200

    Refactor Middlewere

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 var express = require('express');
+var middlewere = require('./middlewere');
 
 var app = express();
 var PORT = 3000;
-
-var middlewere = {
-	
-	requireAuthentication: function(req, res, next) {
-		console.log('Private route hit!!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + 
-					 req.method + req.originalUrl );
-		next();
-	}
-}
 
 app.use(middlewere.logger);
 
